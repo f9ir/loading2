368c9e7d0e6c39f8732b4c615c8c5f828035da2c
f9ir <j4oo@hotmail.com>
2016-02-16 17:40:51 +0000

    Update loading2.js

--- loading2.js
+++ loading2.js
@@ -1,89 +1,35 @@
-(function (lib, img, cjs, ss) {
-
-var p; // shortcut to reference prototypes
-lib.webFontTxtFilters = {}; 
-
-// library properties:
-lib.properties = {
-	width: 400,
-	height: 400,
-	fps: 24,
-	color: "#FFFFFF",
-	webfonts: {},
-	manifest: []
-};
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled-2</title>
+<!-- write your code here -->
 
 
+<script src="https://code.createjs.com/createjs-2015.11.26.min.js"></script>
+<script src="Untitled-2.js"></script>
+<script>
+var canvas, stage, exportRoot;
+function init() {
+	// --- write your JS code here ---
+	
+	canvas = document.getElementById("canvas");
+	exportRoot = new lib.Untitled2();
 
-lib.webfontAvailable = function(family) { 
-	lib.properties.webfonts[family] = true;
-	var txtFilters = lib.webFontTxtFilters && lib.webFontTxtFilters[family] || [];
-	for(var f = 0; f < txtFilters.length; ++f) {
-		txtFilters[f].updateCache();
-	}
-};
-// symbols:
+	stage = new createjs.Stage(canvas);
+	stage.addChild(exportRoot);
+	stage.update();
 
+	createjs.Ticker.setFPS(lib.properties.fps);
+	createjs.Ticker.addEventListener("tick", stage);
+}
 
+</script>
 
-(lib.Symbol2 = function(mode,startPosition,loop) {
-	this.initialize(mode,startPosition,loop,{});
+<!-- write your code here -->
 
-	// Layer 1
-	this.shape = new cjs.Shape();
-	this.shape.graphics.f("#2994B2").s().p("AhoBqQgtgtABg9QgBg9AtgsQAsgrA8gBQA+ABArArQAsAsABA9QgBA9gsAtQgrAsg+AAQg8AAgsgsg");
-	this.shape.setTransform(15,15);
-
-	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
-
-}).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(0,0,30,30);
-
-
-(lib.Symbol1 = function(mode,startPosition,loop) {
-	this.initialize(mode,startPosition,loop,{});
-
-	// Layer 1
-	this.shape = new cjs.Shape();
-	this.shape.graphics.f("#FF5335").s().p("AhoBqQgtgtABg9QgBg9AtgsQAsgrA8gBQA+ABArArQAsAsABA9QgBA9gsAtQgrAsg+AAQg8AAgsgsg");
-	this.shape.setTransform(15,15);
-
-	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
-
-}).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(0,0,30,30);
-
-
-// stage content:
-(lib.Untitled2 = function(mode,startPosition,loop) {
-	this.initialize(mode,startPosition,loop,{});
-
-	// blue copy
-	this.cblue = new lib.Symbol2();
-	this.cblue.setTransform(269,200,1,1,0,0,0,15,15);
-
-	this.timeline.addTween(cjs.Tween.get(this.cblue).wait(1).to({rotation:-12.1,x:259.5},0).wait(1).to({rotation:-24,x:250.2},0).wait(1).to({rotation:-35.7,x:241},0).wait(1).to({rotation:-47.2,x:232},0).wait(1).to({rotation:-58.4,x:223.1},0).wait(1).to({rotation:-69.5,x:214.4},0).wait(1).to({rotation:-80.4,x:205.9},0).wait(1).to({rotation:-91,x:197.5},0).wait(1).to({rotation:-101.5,x:189.3},0).wait(1).to({rotation:-111.7,x:181.3},0).wait(1).to({rotation:-121.7,x:173.4},0).wait(1).to({rotation:-131.5,x:165.7},0).wait(1).to({rotation:-141.2,x:158.1},0).wait(1).to({rotation:-150.6,x:150.7},0).wait(1).to({rotation:-159.8,x:143.5},0).wait(1).to({rotation:-168.8,x:136.4},0).wait(1).to({rotation:-177.6,x:130.5},0).wait(1).to({rotation:-186.2,x:137},0).wait(1).to({rotation:-194.5,x:143.4},0).wait(1).to({rotation:-202.7,x:149.5},0).wait(1).to({rotation:-210.7,x:155.6},0).wait(1).to({rotation:-218.4,x:161.5},0).wait(1).to({rotation:-226,x:167.2},0).wait(1).to({rotation:-233.3,x:172.8},0).wait(1).to({rotation:-240.4,x:178.2},0).wait(1).to({rotation:-247.4,x:183.5},0).wait(1).to({rotation:-254.1,x:188.6},0).wait(1).to({rotation:-260.6,x:193.5},0).wait(1).to({rotation:-266.9,x:198.3},0).wait(1).to({rotation:-273,x:202.9},0).wait(1).to({rotation:-278.9,x:207.4},0).wait(1).to({rotation:-284.6,x:211.8},0).wait(1).to({rotation:-290.1,x:215.9},0).wait(1).to({rotation:-295.4,x:219.9},0).wait(1).to({rotation:-300.4,x:223.8},0).wait(1).to({rotation:-305.3,x:227.4},0).wait(1).to({rotation:-309.9,x:231},0).wait(1).to({rotation:-314.4,x:234.4},0).wait(1).to({rotation:-318.6,x:237.6},0).wait(1).to({rotation:-322.7,x:240.7},0).wait(1).to({rotation:-326.5,x:243.5},0).wait(1).to({rotation:-330.1,x:246.3},0).wait(1).to({rotation:-333.5,x:248.9},0).wait(1).to({rotation:-336.7,x:251.3},0).wait(1).to({rotation:-339.7,x:253.6},0).wait(1).to({rotation:-342.5,x:255.7},0).wait(1).to({rotation:-345.1,x:257.7},0).wait(1).to({rotation:-347.5,x:259.5},0).wait(1).to({rotation:-349.7,x:261.1},0).wait(1).to({rotation:-351.6,x:262.6},0).wait(1).to({rotation:-353.4,x:264},0).wait(1).to({rotation:-354.9,x:265.1},0).wait(1).to({rotation:-356.3,x:266.1},0).wait(1).to({rotation:-357.4,x:267},0).wait(1).to({rotation:-358.3,x:267.7},0).wait(1).to({rotation:-359.1,x:268.3},0).wait(1).to({rotation:-359.6,x:268.7},0).wait(1).to({rotation:-359.9,x:268.9},0).wait(1).to({rotation:-360,x:269},0).wait(1));
-
-	// orange copy
-	this.corange = new lib.Symbol1();
-	this.corange.setTransform(130,200,1,1,0,0,0,15,15);
-
-	this.timeline.addTween(cjs.Tween.get(this.corange).wait(1).to({rotation:12.1,x:139.6},0).wait(1).to({rotation:24,x:149},0).wait(1).to({rotation:35.7,x:158.2},0).wait(1).to({rotation:47.2,x:167.3},0).wait(1).to({rotation:58.4,x:176.2},0).wait(1).to({rotation:69.5,x:185},0).wait(1).to({rotation:80.4,x:193.5},0).wait(1).to({rotation:91,x:202},0).wait(1).to({rotation:101.5,x:210.2},0).wait(1).to({rotation:111.7,x:218.4},0).wait(1).to({rotation:121.7,x:226.3},0).wait(1).to({rotation:131.5,x:234.1},0).wait(1).to({rotation:141.2,x:241.7},0).wait(1).to({rotation:150.6,x:249.2},0).wait(1).to({rotation:159.8,x:256.4},0).wait(1).to({rotation:168.8,x:263.6},0).wait(1).to({rotation:177.6,x:269.7,y:200.5},0).wait(1).to({rotation:186.2,x:265.3,y:207.6},0).wait(1).to({rotation:194.5,x:260.7,y:214.3},0).wait(1).to({rotation:202.7,x:255.8,y:220.5},0).wait(1).to({rotation:210.7,x:250.7,y:226.4},0).wait(1).to({rotation:218.4,x:245.4,y:231.7},0).wait(1).to({rotation:226,x:239.8,y:236.3},0).wait(1).to({rotation:233.3,x:234,y:240.5},0).wait(1).to({rotation:240.4,x:227.9,y:243.8},0).wait(1).to({rotation:247.4,x:221.7,y:246.3},0).wait(1).to({rotation:254.1,x:215.5,y:248},0).wait(1).to({rotation:260.6,x:209.2,y:248.8},0).wait(1).to({rotation:266.9,x:203.1},0).wait(1).to({rotation:273,x:197.2,y:248.2},0).wait(1).to({rotation:278.9,x:191.6,y:247},0).wait(1).to({rotation:284.6,x:186.4,y:245.4},0).wait(1).to({rotation:290.1,x:181.5,y:243.3},0).wait(1).to({rotation:295.4,x:176.9,y:241.1},0).wait(1).to({rotation:300.4,x:172.6,y:238.7},0).wait(1).to({rotation:305.3,x:168.6,y:236.1},0).wait(1).to({rotation:309.9,x:164.9,y:233.6},0).wait(1).to({rotation:314.4,x:161.4,y:231.1},0).wait(1).to({rotation:318.6,x:158.2,y:228.5},0).wait(1).to({rotation:322.7,x:155.2,y:226},0).wait(1).to({rotation:326.5,x:152.4,y:223.5},0).wait(1).to({rotation:330.1,x:149.8,y:221.1},0).wait(1).to({rotation:333.5,x:147.4,y:218.9},0).wait(1).to({rotation:336.7,x:145.2,y:216.7},0).wait(1).to({rotation:339.7,x:143.1,y:214.6},0).wait(1).to({rotation:342.5,x:141.3,y:212.7},0).wait(1).to({rotation:345.1,x:139.6,y:210.9},0).wait(1).to({rotation:347.5,x:138,y:209.2},0).wait(1).to({rotation:349.7,x:136.6,y:207.6},0).wait(1).to({rotation:351.6,x:135.3,y:206.2},0).wait(1).to({rotation:353.4,x:134.2,y:204.9},0).wait(1).to({rotation:354.9,x:133.2,y:203.7},0).wait(1).to({rotation:356.3,x:132.4,y:202.7},0).wait(1).to({rotation:357.4,x:131.7,y:201.9},0).wait(1).to({rotation:358.3,x:131.1,y:201.2},0).wait(1).to({rotation:359.1,x:130.6,y:200.7},0).wait(1).to({rotation:359.6,x:130.3,y:200.3},0).wait(1).to({rotation:359.9,x:130.1,y:200.1},0).wait(1).to({rotation:360,x:130,y:200},0).wait(1));
-
-	// blue
-	this.cblue_1 = new lib.Symbol2();
-	this.cblue_1.setTransform(269,200,1,1,0,0,0,15,15);
-
-	this.timeline.addTween(cjs.Tween.get(this.cblue_1).wait(1).to({rotation:-12.1,x:259.5},0).wait(1).to({rotation:-24,x:250.2},0).wait(1).to({rotation:-35.7,x:241},0).wait(1).to({rotation:-47.2,x:232},0).wait(1).to({rotation:-58.4,x:223.1},0).wait(1).to({rotation:-69.5,x:214.4},0).wait(1).to({rotation:-80.4,x:205.9},0).wait(1).to({rotation:-91,x:197.5},0).wait(1).to({rotation:-101.5,x:189.3},0).wait(1).to({rotation:-111.7,x:181.3},0).wait(1).to({rotation:-121.7,x:173.4},0).wait(1).to({rotation:-131.5,x:165.7},0).wait(1).to({rotation:-141.2,x:158.1},0).wait(1).to({rotation:-150.6,x:150.7},0).wait(1).to({rotation:-159.8,x:143.5},0).wait(1).to({rotation:-168.8,x:136.4},0).wait(1).to({rotation:-177.6,x:130.5},0).wait(1).to({rotation:-186.2,x:137},0).wait(1).to({rotation:-194.5,x:143.4},0).wait(1).to({rotation:-202.7,x:149.5},0).wait(1).to({rotation:-210.7,x:155.6},0).wait(1).to({rotation:-218.4,x:161.5},0).wait(1).to({rotation:-226,x:167.2},0).wait(1).to({rotation:-233.3,x:172.8},0).wait(1).to({rotation:-240.4,x:178.2},0).wait(1).to({rotation:-247.4,x:183.5},0).wait(1).to({rotation:-254.1,x:188.6},0).wait(1).to({rotation:-260.6,x:193.5},0).wait(1).to({rotation:-266.9,x:198.3},0).wait(1).to({rotation:-273,x:202.9},0).wait(1).to({rotation:-278.9,x:207.4},0).wait(1).to({rotation:-284.6,x:211.8},0).wait(1).to({rotation:-290.1,x:215.9},0).wait(1).to({rotation:-295.4,x:219.9},0).wait(1).to({rotation:-300.4,x:223.8},0).wait(1).to({rotation:-305.3,x:227.4},0).wait(1).to({rotation:-309.9,x:231},0).wait(1).to({rotation:-314.4,x:234.4},0).wait(1).to({rotation:-318.6,x:237.6},0).wait(1).to({rotation:-322.7,x:240.7},0).wait(1).to({rotation:-326.5,x:243.5},0).wait(1).to({rotation:-330.1,x:246.3},0).wait(1).to({rotation:-333.5,x:248.9},0).wait(1).to({rotation:-336.7,x:251.3},0).wait(1).to({rotation:-339.7,x:253.6},0).wait(1).to({rotation:-342.5,x:255.7},0).wait(1).to({rotation:-345.1,x:257.7},0).wait(1).to({rotation:-347.5,x:259.5},0).wait(1).to({rotation:-349.7,x:261.1},0).wait(1).to({rotation:-351.6,x:262.6},0).wait(1).to({rotation:-353.4,x:264},0).wait(1).to({rotation:-354.9,x:265.1},0).wait(1).to({rotation:-356.3,x:266.1},0).wait(1).to({rotation:-357.4,x:267},0).wait(1).to({rotation:-358.3,x:267.7},0).wait(1).to({rotation:-359.1,x:268.3},0).wait(1).to({rotation:-359.6,x:268.7},0).wait(1).to({rotation:-359.9,x:268.9},0).wait(1).to({rotation:-360,x:269},0).wait(1));
-
-	// orange
-	this.corange_1 = new lib.Symbol1();
-	this.corange_1.setTransform(130,200,1,1,0,0,0,15,15);
-
-	this.timeline.addTween(cjs.Tween.get(this.corange_1).wait(1).to({rotation:12.1,x:139.6},0).wait(1).to({rotation:24,x:149},0).wait(1).to({rotation:35.7,x:158.2},0).wait(1).to({rotation:47.2,x:167.3},0).wait(1).to({rotation:58.4,x:176.2},0).wait(1).to({rotation:69.5,x:185},0).wait(1).to({rotation:80.4,x:193.5},0).wait(1).to({rotation:91,x:202},0).wait(1).to({rotation:101.5,x:210.2},0).wait(1).to({rotation:111.7,x:218.4},0).wait(1).to({rotation:121.7,x:226.3},0).wait(1).to({rotation:131.5,x:234.1},0).wait(1).to({rotation:141.2,x:241.7},0).wait(1).to({rotation:150.6,x:249.2},0).wait(1).to({rotation:159.8,x:256.4},0).wait(1).to({rotation:168.8,x:263.6},0).wait(1).to({rotation:177.6,x:269.5},0).wait(1).to({rotation:186.2,x:263},0).wait(1).to({rotation:194.5,x:256.6},0).wait(1).to({rotation:202.7,x:250.3},0).wait(1).to({rotation:210.7,x:244.2},0).wait(1).to({rotation:218.4,x:238.3},0).wait(1).to({rotation:226,x:232.5},0).wait(1).to({rotation:233.3,x:226.9},0).wait(1).to({rotation:240.4,x:221.4},0).wait(1).to({rotation:247.4,x:216.2},0).wait(1).to({rotation:254.1,x:211},0).wait(1).to({rotation:260.6,x:206},0).wait(1).to({rotation:266.9,x:201.2},0).wait(1).to({rotation:273,x:196.6},0).wait(1).to({rotation:278.9,x:192},0).wait(1).to({rotation:284.6,x:187.7},0).wait(1).to({rotation:290.1,x:183.5},0).wait(1).to({rotation:295.4,x:179.4},0).wait(1).to({rotation:300.4,x:175.6},0).wait(1).to({rotation:305.3,x:171.8},0).wait(1).to({rotation:309.9,x:168.3},0).wait(1).to({rotation:314.4,x:164.9},0).wait(1).to({rotation:318.6,x:161.6},0).wait(1).to({rotation:322.7,x:158.6},0).wait(1).to({rotation:326.5,x:155.6},0).wait(1).to({rotation:330.1,x:152.9},0).wait(1).to({rotation:333.5,x:150.3},0).wait(1).to({rotation:336.7,x:147.8},0).wait(1).to({rotation:339.7,x:145.5},0).wait(1).to({rotation:342.5,x:143.4},0).wait(1).to({rotation:345.1,x:141.4},0).wait(1).to({rotation:347.5,x:139.6},0).wait(1).to({rotation:349.7,x:137.9},0).wait(1).to({rotation:351.6,x:136.4},0).wait(1).to({rotation:353.4,x:135.1},0).wait(1).to({rotation:354.9,x:133.9},0).wait(1).to({rotation:356.3,x:132.9},0).wait(1).to({rotation:357.4,x:132},0).wait(1).to({rotation:358.3,x:131.3},0).wait(1).to({rotation:359.1,x:130.7},0).wait(1).to({rotation:359.6,x:130.3},0).wait(1).to({rotation:359.9,x:130.1},0).wait(1).to({rotation:360,x:130},0).wait(1));
-
-}).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(315,385,169,30);
-
-})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{});
-var lib, images, createjs, ss;
+</head>
+<body onload="init();" style="background-color:#D4D4D4;margin:0px;">
+	<canvas id="canvas" width="400" height="400" style="background-color:#FFFFFF"></canvas>
+</body>
+</html>
